a91e6b663b8e3207c3f02bdb844cfa77137e2df2
Ozzy Gomes <ozzygomes@outlook.com>
2022-08-30 19:58:39 -0300

    analyse fraud detection

---
+++ Online Payments Fraud Detection.py
@@ -0,0 +1,51 @@
+from statistics import correlation
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+#lê o arquivo das transações
+df = pd.read_csv('credit card.csv')
+
+# verifica se existe algum valor nulo 
+df[df.isnull().sum(axis=1)>0]
+
+# Explorando tipos de transação 
+type = df['type'].value_counts()
+transactions = type.index
+quantity = type.values
+
+#criando gráfico de trasações
+figure = px.pie(df, values=quantity, names=transactions, title="Distribuição dos tipos de Transações")
+figure.show()
+
+# verificando correlação entre os dados e a coluna isFraud
+correlation = df.corr()
+correlation['isFraud'].sort_values(ascending=False)
+
+# com a função map() altero 'type' de dados categoricos para dados numericos 
+df['type'] = df['type'].map({'CASH_OUT': 1, 'PAYMENT': 2,
+                             'CASH_IN': 3, 'TRANSFER': 4,
+                             'DEBIT': 5})
+
+# na coluna 'isFraud' faço o oposto
+df['isFraud'] = df['isFraud'].map({0: 'No Fraud', 1: 'Fraud'})
+
+df.head()
+
+# separando os dados em dados de treino e dados de teste
+
+x = np.array(df[['type', 'amount', 'oldbalanceOrg', 'newbalanceOrig']])
+y = np.array(df[['isFraud']])
+
+# treinando o modelo
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.10, random_state=42)
+model = DecisionTreeClassifier()
+model.fit(xtrain, ytrain)
+model.score(xtest, ytest)
+
+#predição
+#features = [type, amount, oldbalanceOrg, newbalanceOrig]
+features = np.array([[4, 9000.60, 9000.60, 0.0]])
+model.predict(features)
